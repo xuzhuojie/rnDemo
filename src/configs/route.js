f4eab31934da90218f31ec7985c3f56311f8d114
Jerry <xuzj@mogulinker.com>
2023-05-06 15:33:14 +0800

    feat: add rneui

--- src/configs/route.js
+++ src/configs/route.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../pages/home';
 import Login from '../pages/login';
+import { Icon } from '@rneui/themed';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,8 +11,16 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName="Home">
-      {/*<Tab.Navigator>*/}
-        <Tab.Screen name="Home" component={Home} />
+        {/*<Tab.Navigator>*/}
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: () => (
+              <Icon name="sc-telegram" type="evilicon" color="#517fa4" />
+            ),
+          }}
+        />
         <Tab.Screen name="Login" component={Login} />
       </Tab.Navigator>
     </NavigationContainer>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { Icon } from '@rneui/themed';
 
 function Home(props) {
   useEffect(() => {
@@ -9,6 +10,8 @@ function Home(props) {
   return (
     <View>
       <Text>Home</Text>
+      <Icon name="sc-telegram" type="evilicon" color="#517fa4" />
+      <Icon name="rowing" />
     </View>
   );
 }
